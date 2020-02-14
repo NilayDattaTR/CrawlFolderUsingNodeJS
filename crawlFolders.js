454e5778e78e141e66fa57a44b2753cf0837998b
nilaydatta <nilay.datta@live.com>
2020-02-14 15:47:09 +0530

    modifying function to add text files containing string of file

--- crawlFolders.js
+++ crawlFolders.js
@@ -3,10 +3,23 @@ const joinPath = require("path.join");
 const root = __dirname + '/Files';
 
 function crawlFiles() {
-    const list = fs.readdirSync(root);
-    const filepath = list.map(el => {
-        return '/' + el + '/index.html';
-    })
-    console.log(filepath);
+    if(fs.existsSync(root)) {
+        const list = fs.readdirSync(root);
+        const filepath = list.map(el => {
+            return '/' + el + '/index.html';
+        })
+        const stringData = filepath.join('\n');
+        console.log(stringData);
+        fs.writeFile('links.txt', stringData, function(err) {
+            if (err) {
+                failure(err);
+            } else {
+                console.log("File saved successfully!");
+            }
+        });
+    } else {
+        alert('Files folder is missing !!');
+    }
+    
 }
 crawlFiles();
