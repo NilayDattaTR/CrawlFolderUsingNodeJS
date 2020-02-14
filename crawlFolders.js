743d9cc182e6739302823daa650f786a927ca88d
nilaydatta <nilay.datta@live.com>
2020-02-14 16:16:02 +0530

    final code

--- crawlFolders.js
+++ crawlFolders.js
@@ -6,19 +6,19 @@ function crawlFiles() {
     if(fs.existsSync(root)) {
         const list = fs.readdirSync(root);
         const filepath = list.map(el => {
-            return '/' + el + '/index.html';
+            return 'https://github.com/NilayDattaTR/CrawlFolderUsingNodeJS/tree/master/Files/' + el + '/index.html';
         })
         const stringData = filepath.join('\n');
         console.log(stringData);
         fs.writeFile('links.txt', stringData, function(err) {
             if (err) {
-                failure(err);
+                console.log('Some Error Occured while saving links.txt !');
             } else {
                 console.log("File saved successfully!");
             }
         });
     } else {
-        alert('Files folder is missing !!');
+        console.log('Files folder is missing !!');
     }
     
 }
